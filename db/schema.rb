a744e93d8f8a6dc2e90c485a76377511ba53af3e
Chris McCarthy <chmc87@gmail.com>
2022-11-15 12:42:04 +0100

    change completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_104211) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_111437) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "details"
-    t.string "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
